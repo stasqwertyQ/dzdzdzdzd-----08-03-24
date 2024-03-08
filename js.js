7229a651b7e22fa553ae586aea1225fa36967573
stasqwertyQ <125751548+stasqwertyQ@users.noreply.github.com>
2024-03-08 21:16:56 +0200

    fef

---
+++ js.js
@@ -0,0 +1,123 @@
+///task1
+
+
+function Account({ login, email }) {
+  this.login = login;
+  this.email = email;
+}
+
+Account.prototype.getInfo = function() {
+  console.log(`Login: ${this.login}, Email: ${this.email}`);
+};
+
+// console.log(Account.prototype.getInfo); 
+
+const mango = new Account({
+  login: 'Mangozedog',
+  email: 'mango@dog.woof',
+});
+
+mango.getInfo(); 
+
+const poly = new Account({
+  login: 'Poly',
+  email: 'poly@mail.com',
+});
+
+poly.getInfo(); 
+
+
+
+
+// task 2
+
+
+
+
+
+/// task 3
+
+
+
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    const index = this.items.indexOf(item);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+    }
+  }
+}
+
+const storage = new Storage([
+  'Нанітоіди',
+  'Пролонгер',
+  'Залізні жупи',
+  'Антигравітатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+
+storage.addItem('Дроїд');
+console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+
+
+
+
+//// taSK 4
+
+
+
+class StringBuilder {
+  constructor(strr) {
+    this._value = strr;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  append(strr) {
+    this._value += strr;
+  }
+
+  prepend(strr) {
+    this._value = strr + this._value;
+  }
+
+  pad(strr) {
+    this._value = strr + this._value + strr;
+  }
+}
+
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
+
+
+
+
+
